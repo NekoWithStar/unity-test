4defe758c40afafb4e2ff6a4d7aa15d8946af063
nekowithstar-admin <huangqiongtong@gmail.com>
2024-07-24 16:22:45 +0800

    修复 Rotate.cs 中yAngle变量无限自增

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour
+{
+    public float yAngle = 180.0f;
+    public float rotateSpeed = 1.0f;
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up, yAngle * rotateSpeed * Time.deltaTime);
+    }
+}
