f0b893f81877d014e89b22a76b6a514be41421d5
Urimo1 <fuyifull@163.com>
2024-07-24 09:33:08 +0800

    添加了旋转方块

---
+++ Assets/sscript/rotate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private float y = 180;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.eulerAngles = new Vector3(0, y++, 0);
+    }
+}
