2839288fd512a2603a8df61af3603dd8b31e6780
CutrelyAlex <huangqiongtong@gmail.com>
2024-08-27 21:22:32 +0800

    no message

---
+++ Assets/Learning/Qf.TypeEventSystemExample/TypeEventSystemExample.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace QFramework.Example
+{
+
+    public interface IEventA
+    {
+
+    }
+    public struct EventA : IEventA
+    {
+        public int Count;
+    }
+
+    public class TypeEventSystemExample : MonoBehaviour
+    {
+        private void Start()
+        {
+            TypeEventSystem.Global.Register<IEventA>(e => {
+                if(e is EventA)
+                {
+                    Debug.Log("event is received");
+                }
+            }).UnRegisterWhenGameObjectDestroyed(gameObject);
+        }
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                TypeEventSystem.Global.Send<IEventA>(new EventA()
+                {
+                    Count = 0
+                });
+            }
+        }
+    }
+}
